f6efc9d3e033d7c42a4d006fc1c9d0b1ebef4f2b
Shreyansh Singh <sriyanshdon2005@gmail.com>
2023-10-24 13:50:23 +0530

    new

--- Upload.cpp
+++ Upload.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Hello World";  //This is Newely Added
+    while (true)
+    {
+        cout<<"Hello World";
+    }
+    
     return 0;
 }
---
+++ demo.py
@@ -0,0 +1,4 @@
+print("Hello World")
+i=12
+while (i<19):
+    print("\033[31mThis is red text.")
